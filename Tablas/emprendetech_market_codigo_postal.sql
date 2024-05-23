7e0b3555df71f0afe73ec8141f3188fe21e11409
tovariddt <domingueztovarisraeldavid99@gmail.com>
2024-05-22 19:20:28 -0600

    Datos
    Nuevo 22 de mayo

---
+++ Tablas/emprendetech_market_codigo_postal.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: emprendetech_market
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `codigo_postal`
+--
+
+DROP TABLE IF EXISTS `codigo_postal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `codigo_postal` (
+  `id_codigo_postal` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `clave` char(5) NOT NULL,
+  `clave_estado` char(2) NOT NULL,
+  `nombre_estado` varchar(150) NOT NULL,
+  `clave_municipio` char(3) NOT NULL,
+  `nombre_municipio` varchar(150) NOT NULL,
+  `clave_localidad` char(4) NOT NULL,
+  `nombre_localidad` varchar(150) NOT NULL,
+  `tipo_localidad` varchar(150) NOT NULL,
+  `clave_ciudad` char(4) NOT NULL,
+  `nombre_ciudad` varchar(150) NOT NULL,
+  `zona` varchar(50) NOT NULL,
+  `id_pais` int unsigned NOT NULL,
+  `creadoridusuario` int DEFAULT '1',
+  `fechacreacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `fechamodificacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_codigo_postal`),
+  KEY `fk_id_pais` (`id_pais`),
+  KEY `idx_clave` (`clave`),
+  CONSTRAINT `fk_id_pais` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id_pais`)
+) ENGINE=InnoDB AUTO_INCREMENT=145422 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-22 19:15:16
